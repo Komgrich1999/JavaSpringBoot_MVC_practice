f74adf3c6b705d1e4efa0f022a36f0f01c426afd
KOMGRICH SRISAK <6131305035@lamduan.mfu.ac.th>
2021-11-08 02:49:36 +0700

    start

---
+++ src/main/java/com/Spring_MVC_practice1/Spring_MVC_practice1/SpringMvcPractice1Application.java
@@ -0,0 +1,12 @@
+package com.Spring_MVC_practice1.Spring_MVC_practice1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringMvcPractice1Application {
+
+	public static void main(String[] args) { SpringApplication.run(SpringMvcPractice1Application.class, args); }
+}
---
+++ src/test/java/com/Spring_MVC_practice1/Spring_MVC_practice1/SpringMvcPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_MVC_practice1.Spring_MVC_practice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
